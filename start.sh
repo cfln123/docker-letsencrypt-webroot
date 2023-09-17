39a6bc58bcb52f79b9bbef4b2633f2e874e4194e
cfln123 <claudio.ferreira.raizes@gmail.com>
2023-09-17 00:47:35 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@ if [[ $STAGING -eq 1 ]]; then
   ADDITIONAL="--staging"
 fi
 
-DARRAYS=(${DOMAINS})
+DARRAYS=$(echo $DOMAINS | sed 's/*/wildcard/g')
 EMAIL_ADDRESS=${EMAIL}
 LE_DOMAINS=("${DARRAYS[*]/#/-d }")
 
@@ -69,7 +69,7 @@ le_renew() {
 }
 
 le_check() {
-    cert_file="/etc/letsencrypt/live/$(echo $DARRAYS | sed 's/*/wildcard/g')/fullchain.pem"
+    cert_file="/etc/letsencrypt/live/$DARRAYS/fullchain.pem"
 
     if [[ -e $cert_file ]]; then
 
